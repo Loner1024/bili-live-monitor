0da634c24c3070bbaf7173e1d6e9e27494d077ce
Loner1024 <xth12138@gmail.com>
2024-07-12 21:19:08 +0800

    refactor: :recycle: table name with s3 prefix

--- utils/src/utils.rs
+++ utils/src/utils.rs
@@ -3,7 +3,7 @@ use anyhow::{anyhow, Result};
 use chrono::{Local, TimeZone, Utc};
 
 // 获取表名
-pub fn get_table_name(message_type: MessageType, room_id: i64, timestamp: i64) -> Result<String> {
+pub fn get_table_name(bucket: &str, message_type: MessageType, room_id: i64, timestamp: i64) -> Result<String> {
     // 将 Unix 时间戳转换为 UTC 时间
     // 将 UTC 时间转换为本地时间
     let datetime = Utc
@@ -12,7 +12,7 @@ pub fn get_table_name(message_type: MessageType, room_id: i64, timestamp: i64) -
         .ok_or(anyhow!("Invalid timestamp"))?
         .with_timezone(&Local).format("%Y-%m-%d");
 
-    Ok(format!("{}/{}/{}.parquet", datetime, room_id, message_type))
+    Ok(format!("s3://{}/{}/{}/{}.parquet", bucket, datetime, room_id, message_type))
 }
 
 #[cfg(test)]
@@ -21,11 +21,12 @@ mod test {
 
     #[test]
     fn test_get_table_name() {
+        let bucket_name = "bilibili";
         let message_type = MessageType::Danmu;
         let room_id = 123456789;
         let timestamp = 1720526217; // 2023-05-10 12:00:00 UTC
 
-        let table_name = get_table_name(message_type, room_id, timestamp).unwrap();
-        assert_eq!(table_name, "2024-07-09/123456789/danmu.parquet");
+        let table_name = get_table_name(bucket_name, message_type, room_id, timestamp).unwrap();
+        assert_eq!(table_name, "s3://bilibili/2024-07-09/123456789/danmu.parquet");
     }
 }
