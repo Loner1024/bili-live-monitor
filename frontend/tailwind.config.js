1c05283b1f434e58552c35acdb60ff4bd7e5956d
Loner1024 <xth12138@gmail.com>
2024-07-14 22:28:09 +0800

    refactor: :recycle: remove frontend

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["*.html", "./frontend/src/**/*.rs", "./frontend/src/*.rs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
