17b706f88fa6b617b705e0abdf732301f1b5595f
Loner1024 <xth12138@gmail.com>
2024-07-20 02:16:46 +0800

    style: :lipstick: fix lint

--- crawler/src/storage.rs
+++ crawler/src/storage.rs
@@ -154,7 +154,7 @@ impl<'a> Storage<'a> {
         self.flush()?;
         // change timestamp
         self.timestamp = timestamp;
-        Self::init_table(&self.conn, &self.bucket, self.room_id, timestamp)?;
+        Self::init_table(self.conn, &self.bucket, self.room_id, timestamp)?;
         Ok(())
     }
 }
