aee657dd132fdfbd42c26526de45799111307414
Loner1024 <xth12138@gmail.com>
2024-07-24 17:09:43 +0800

    feat: :sparkles: grace shutdown

--- crawler/src/bin/danmu.rs
+++ crawler/src/bin/danmu.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use base64::Engine;
 use chrono::Utc;
 use crawler::storage::Storage;
@@ -6,6 +6,8 @@ use danmu_client::danmu::Client;
 use duckdb::Connection;
 use log::{debug, error, info};
 use parse::Message;
+use tokio::signal;
+use tokio::sync::watch;
 use tokio::task::LocalSet;
 use utils::utils::{get_rooms, is_new_day};
 
@@ -21,6 +23,25 @@ async fn main() -> Result<()> {
 
     let local_set = LocalSet::new();
 
+    let (shutdown_tx, shutdown_rx) = watch::channel(());
+
+    let shutdown_signal = tokio::spawn(async move {
+        let sigint = signal::ctrl_c();
+        let mut sigterm = signal::unix::signal(signal::unix::SignalKind::terminate())?;
+
+        tokio::select! {
+            _ = sigint => {
+                info!("Received SIGINT, shutting down...");
+            },
+            _ = sigterm.recv() => {
+                info!("Received SIGTERM, shutting down...");
+            },
+        }
+
+        let _ = shutdown_tx.send(());
+        Result::<(), anyhow::Error>::Ok(())
+    });
+
     local_set
         .run_until(async move {
             let mut tasks = Vec::new();
@@ -34,22 +55,32 @@ async fn main() -> Result<()> {
                         return;
                     }
                 };
+                let shutdown_rx = shutdown_rx.clone();
                 let task = tokio::task::spawn_local(async move {
-                    if let Err(e) = process_room(room_id, cookies, conn).await {
+                    if let Err(e) = process_room(room_id, cookies, conn, shutdown_rx).await {
                         error!("Error processing room {}: {:?}", room_id, e);
                     }
                 });
                 tasks.push(task);
             }
 
+            // 等待所有任务完成
             futures::future::join_all(tasks).await;
         })
         .await;
 
+    // 等待终止信号任务完成
+    shutdown_signal.await??;
+
     Ok(())
 }
 
-async fn process_room(room_id: i64, cookies: String, conn: Connection) -> Result<()> {
+async fn process_room(
+    room_id: i64,
+    cookies: String,
+    conn: Connection,
+    mut shutdown_rx: watch::Receiver<()>,
+) -> Result<()> {
     let mut start_time = Utc::now();
 
     // 创建 Storage 实例
@@ -60,25 +91,43 @@ async fn process_room(room_id: i64, cookies: String, conn: Connection) -> Result
 
     info!("开始监听 room_id: {}", room_id);
 
-    while let Some(message) = rx.recv().await {
-        let now = Utc::now();
-        if is_new_day(start_time.timestamp(), now.timestamp())? {
-            start_time = now;
-            storage.switch_new_date(now.timestamp())?;
-            info!("切换到新的日期: {}", now);
-        }
-        match message {
-            Message::Danmu(msg) => {
-                storage.create_danmu_message(msg)?;
-            }
-            Message::EnterRoom(_) => {}
-            Message::OnlineCount(_) => {}
-            Message::SuperChat(msg) => {
-                storage.create_super_chat_message(msg)?;
-            }
-            Message::Default => {}
+    loop {
+        tokio::select! {
+            message = rx.recv() => {
+                if let Some(message) = message {
+                    let now = Utc::now();
+                    if is_new_day(start_time.timestamp(), now.timestamp())? {
+                        start_time = now;
+                        storage.switch_new_date(now.timestamp())?;
+                        info!("切换到新的日期: {}", now);
+                    }
+                    match message {
+                        Message::Danmu(msg) => {
+                            storage.create_danmu_message(msg)?;
+                        }
+                        Message::EnterRoom(_) => {}
+                        Message::OnlineCount(_) => {}
+                        Message::SuperChat(msg) => {
+                            storage.create_super_chat_message(msg)?;
+                        }
+                        Message::Default => {}
+                    }
+                }
+            },
+            _ = shutdown_rx.changed() => {
+                // 收到 shutdown 信号，退出循环
+                info!("收到 shutdown 信号，停止监听 room_id: {}", room_id);
+                break;
+            },
         }
     }
+
+    // 执行清理工作
+    info!("开始清理 room_id: {}", room_id);
+    storage
+        .flush()
+        .map_err(|e| anyhow!("清理 room {} 出错: {}", room_id, e))?;
+    info!("清理 room {} 完成, Bye!", room_id);
 
     Ok(())
 }
