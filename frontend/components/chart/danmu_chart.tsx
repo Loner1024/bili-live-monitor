a1ec9f0500354970132c09549379546bd572d945
Loner1024 <xth12138@gmail.com>
2024-08-21 18:59:21 +0800

    refactor: :recycle: text

--- frontend/components/chart/danmu_chart.tsx
+++ frontend/components/chart/danmu_chart.tsx
@@ -96,9 +96,9 @@ export function DanmuChart(params: {room_id: string}) {
         <Card>
             <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
                 <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
-                    <CardTitle>Line Chart - Interactive</CardTitle>
+                    <CardTitle>一些统计数据</CardTitle>
                     <CardDescription>
-                        Showing total visitors for the last 3 months
+                        并不靠谱
                     </CardDescription>
                 </div>
                 <div className="flex">
